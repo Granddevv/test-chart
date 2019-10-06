a03cca8c00adfe2d073a44281c1c63c0e2ceea55
granddevv <spacingflight@outlook.com>
2019-10-06 11:03:44 -0600

    Add Analyze module & routing

---
+++ src/app/page/analyze/analyze.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AnalyzeRoutingModule } from './analyze-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    AnalyzeRoutingModule
+  ]
+})
+export class AnalyzeModule { }
